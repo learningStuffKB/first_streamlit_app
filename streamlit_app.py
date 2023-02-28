5454fa6b9f1425a99712374845a19ac0f031c474
learningStuffKB <126510773+learningStuffKB@users.noreply.github.com>
2023-02-28 18:45:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
+
 
 streamlit.header("The fruit load contains ")
 def get_fruit_load_list():
@@ -56,7 +56,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
- 
+ streamlit.stop()
 
 
 
